f124c0781e623830b71a8aec7da76fccf8fa4e28
levandung-it03 <levandung.it03@gmail.com>
2024-10-13 23:50:26 +0700

    Temp Backup

--- src/pages/ManageUserInfoPage/UserInfoTable/SubscriptionsDialog/SubscriptionsDialog.js
+++ src/pages/ManageUserInfoPage/UserInfoTable/SubscriptionsDialog/SubscriptionsDialog.js
@@ -1,4 +1,80 @@
+import { useSelector } from 'react-redux';
+import './SubscriptionsDialog.scss';
+import { SubscriptionAdminThunk } from '~/redux/thunks/subscriptionThunk';
+import { Input } from '~/components';
+import Pagination from '~/components/ui/Table/Pagination/Pagination';
+import { addToast } from '~/redux/slices/toastSlice';
+import { Table, FormatterDict } from '~/components/ui/Table/CustomTable';
+import { useMemo } from 'react';
 
 export default function SubscriptionsDialog({ userInfoId }) {
+    const subscriptionsState = useSelector((state) => state.subscription)
+    const [sortData, setSortData] = useState(null);
+    const [filterData, setFilterData] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    
+    const tableComponents = useMemo(() => {
+        return FormatterDict.TableComponent(
+            columnsInfo=[
+                FormatterDict.ColumnInfo("firstName", "First Name"),
+                FormatterDict.ColumnInfo("lastName", "Last Name"),
+                FormatterDict.ColumnInfo("subscribedTime", "Subscribed Time"),
+                FormatterDict.ColumnInfo("efficientDays", "Efficient Days"),
+                FormatterDict.ColumnInfo("scheduleName", "Schedule Name"),
+                FormatterDict.ColumnInfo("scheduleCoins", "Schedule Coins"),
+                FormatterDict.ColumnInfo("completedTime", "Completed Time"),
+            ],
+            filterFields=[
+                FormatterDict.FilterField("firstName", <Input name="fisrtName" />),
+                FormatterDict.FilterField("lastName", <Input name="lastName" />),
+                FormatterDict.FilterField("fromSubscribedTime", <Input type="datetime-local" name="fromSubscribedTime" />),
+                FormatterDict.FilterField("toSubscribedTime", <Input type="datetime-local" name="toSubscribedTime" />),
+                FormatterDict.FilterField("efficientDays", <Input type="number" name="efficientDays" />),
+                FormatterDict.FilterField("scheduleName", <Input name="scheduleName" />),
+                FormatterDict.FilterField("scheduleCoins", <Input type="number" name="scheduleCoins" />),
+                FormatterDict.FilterField("fromCompletedTime", <Input type="datetime-local" name="fromCompletedTime" />),
+                FormatterDict.FilterField("toCompletedTime", <Input type="datetime-local" name="toCompletedTime" />),
+            ],
+            sortingFields=[
+                FormatterDict.ColumnInfo("firstName", "First Name"),
+                FormatterDict.ColumnInfo("lastName", "Last Name"),
+                FormatterDict.ColumnInfo("subscribedTime", "Subscribed Time"),
+                FormatterDict.ColumnInfo("efficientDays", "Efficient Days"),
+                FormatterDict.ColumnInfo("scheduleName", "Schedule Name"),
+                FormatterDict.ColumnInfo("scheduleCoins", "Schedule Coins"),
+                FormatterDict.ColumnInfo("completedTime", "Completed Time"),
+            ],
+        );
+    }, []);
 
+    const tableStates = useMemo(() => {
+        return FormatterDict.TableStates(subscriptionsState, currentPage, addToast, {
+                moreParams: { id: userInfoId },
+                GET_thunk: SubscriptionAdminThunk.getAllSubscriptionByUserInfoThunk,
+            },
+            { sortData, setSortData },
+            { filterData, setFilterData },
+        );
+    }, []);
+
+    const tableModes = useMemo(() => {
+        return FormatterDict.TableModes(false, false, false, false);
+    }, []);
+
+    return (
+        <>
+            <Table
+                className="user-info-table"
+                title="User Information"
+                tableComponents={{...tableComponents}}
+                tableStates={{...tableStates}}
+                tableModes={{...tableModes}}
+            />
+            <Pagination
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+                totalPages={subscriptionsState.totalPages}
+            />
+        </>
+    );
 }
--- src/redux/slices/userInfoSlice.js
+++ src/redux/slices/userInfoSlice.js
@@ -18,52 +18,14 @@ const userInfoSlice = createSlice({
                 state.loading = true;
             })
             .addCase(UserInfoAdminThunk.getAllUserInfoThunk.fulfilled, (state, action) => {
-                const data = {
-                    data: [],
-                    currentPage: 1,
-                    totalPages: 1,
-                    message: "Succeed"
-                }
-                for (let ind = 0; ind <= 40; ind++)
-                    data.data.push({
-                        userInfoId: ind,
-                        firstName: "Dung",
-                        lastName: "Le Van",
-                        gender: "MALE",
-                        email: "user@gmail.com",
-                        coins: 2000,
-                        dob: [
-                            2003,
-                            12,
-                            11
-                        ],
-                        userId: 2,
-                        createdTime: [
-                            2024,
-                            10,
-                            11,
-                            10,
-                            0,
-                            0,
-                            895959000
-                        ],
-                        active: ind%2==0
-                    });
                 state.loading = false;
-                // state.data = action.payload.data.data.map(objData => ({
-                //     ...objData,
-                //     dob: new Date(...objData.dob).toISOString().slice(0, 10),
-                //     createdTime: new Date(...objData.createdTime).toISOString(),
-                // }));
-                // state.totalPages = action.payload.data.totalPages;
-                // state.message = action.payload.message;
-                state.data = data.data.map(objData => ({
+                state.data = action.payload.data.data.map(objData => ({
                     ...objData,
                     dob: new Date(...objData.dob).toISOString().slice(0, 10),
                     createdTime: new Date(...objData.createdTime).toISOString(),
-                }));;
-                state.totalPages = data.totalPages;
-                state.message = data.message;
+                }));
+                state.totalPages = action.payload.data.totalPages;
+                state.message = action.payload.message;
             })
             .addCase(UserInfoAdminThunk.getAllUserInfoThunk.rejected, (state) => {
                 state.loading = false;
@@ -76,14 +38,19 @@ const userInfoSlice = createSlice({
             })
             .addCase(UserInfoAdminThunk.updateUserStatusThunk.fulfilled, (state, action) => {
                 state.loading = false;
+                for (var ind = 0; ind < state.data.length; ind++) {
+                    if (state.data[ind]["userId"] == action.payload.data["userId"]) {
+                        state.data[ind]["active"] = action.payload.data["newStatus"];
+                        break;
+                    }
+                }
                 state.message = action.payload.message;
             })
             .addCase(UserInfoAdminThunk.updateUserStatusThunk.rejected, (state, action) => {
                 state.loading = false;
                 state.message = action.payload.message;
-            });
+            });        
     },
 });
 
-export const { toggleSelectRow, selectAllRows } = userInfoSlice.actions;
 export default userInfoSlice.reducer;
